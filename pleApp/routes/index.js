46960079e696f27c9b8040fcd9f76da8e4384dc1
nezoutcarl <nezout.carlos@gmail.com>
2019-01-29 20:37:03 +0100

    app visualisation

--- pleApp/routes/index.js
+++ pleApp/routes/index.js
@@ -1,24 +1,57 @@
-var express = require('express');
-var router = express.Router();
-var hbase = require('hbase');
+let express = require('express');
+let router = express.Router();
+let app = require('../app.js');
+let path = require('path');
+let hbase = require('hbase-rpc-client');
 
 //instantiate client 
-const client = hbase({host: '10.0.5.25', port: 2181})
-
-client.tables((error, tables) => {
-  console.log('OKOK')
-  console.info(tables)
-})
-
+const client = hbase({
+    zookeeperHosts: ['young'] ,
+    zookeeperRoot: '/hbase'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'PLE Project App' });  
+}); 
+ 
+/* GET the map page */
+router.get('/map', function(req, res, next) {  
+  res.render('map', { title: 'Map Page' });
 });
 
-/* GET the map page */
-router.get('/map', function(req, res, next) {
-  res.render('map', { title: 'Map Page' });
+/* GET Specific image from HBASE TABLE DB */
+router.get('/image/:z/:x/:y', (req, res, next) => {  
+    if (req.params.x && req.params.y && req.params.z) {
+      //let rowID = req.params.x +','+ (180-Number(req.params.y));
+        let key = { "z": parseInt(req.params.z),
+            "x":  parseInt(req.params.x)+256*Math.pow(2,parseInt(req.params.z)),
+            "y":  parseInt(req.params.y)+256*Math.pow(2,parseInt(req.params.z))};
+    
+        rowKey =key.z+'/'+key.y+'/'+key.x
+        //rowKey ='3/-12/10'
+        get = new hbase.Get(rowKey)
+        key = { "z": parseInt(req.params.z),
+            "x":  parseInt(req.params.x),
+            "y":  parseInt(req.params.y)};
+    
+        client.get('famane', get,function (err,value1) { 
+            if (err) {
+                res.sendFile(path.join(__dirname, '../public/default.png'))
+            }
+            else {
+                if(value1 !== null){
+                      
+                    let val = value1.columns[0].value;
+                    console.log(val);
+                    let data = new Buffer(val, 'base64');
+                    res.contentType('image/jpeg');
+                    res.send(data);
+                }          
+            }
+               
+        });          
+    }
 });
 
 module.exports = router;
